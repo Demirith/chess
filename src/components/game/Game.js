ee235b3ef68f38906a87086719dfb1bc918ef4c0
James Kytösaho <james.kytosaho@academicwork.se>
2020-01-23 17:17:58 +0100

    refactor handling of starting position

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -1,123 +1,21 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Game.css';
-import Board from  "../board/Board";
+import Board from "../board/Board";
 import VerticalDescription from "../verticalDescription/VerticalDescription";
 import HorizontalDescription from "../horizontalDescription/HorizontalDescription";
-import { pawnRules, knightRules, bishopRules, kingRules, rookRules } from "./utils/GameUtils"; 
-import Square from '../square/Square';
+import { pawnRules, knightRules, bishopRules, kingRules, rookRules, getStartPosition, pieceTypes } from "./GameUtils"; 
 
 function Game() {
-  const [pieces, setPieces] = useState([]); //Rename to pieces history
+  const [pieces, setPieces] = useState([]); //Rename to pieces history or position?
   const [initialized, setInitialized] = useState(false);
-
-  const pieceTypes = {
-    Pawn: 1,
-    Rook: 2,
-    Knight: 3,
-    Bishop: 4,
-    Queen: 5,
-    King: 6
-  };
-
-  const getPiecesDefaultsquareId = useCallback(
-    () => {
-      const boardLetters = ["a", "b", "c", "d", "e", "f", "g", "h"];
-      const boardNumbers = [8, 7, 2, 1];
-      let boardIdLetterIndex = 0;
-      let boardIdNumbersIndex = 0;
-      let id;
-      let type;
-      let squareId;
-  
-      for (let index = 1; index <= 32; index++) {
-        let isWhite = true;
-        squareId = boardNumbers[boardIdNumbersIndex] + boardLetters[boardIdLetterIndex];
-        const positionY = boardNumbers[boardIdNumbersIndex];
-        const positionX = boardIdLetterIndex + 1;
-
-        if (squareId === "1a") {
-          id = "rook" + squareId;
-        } else if (squareId === "1b") {
-          id = "knight" + squareId;
-        } else if (squareId === "1c") {
-          id = "bishop" + squareId;
-        } else if (squareId === "1d") {
-          id = "queen" + squareId;
-        } else if (squareId === "1e") {
-          id = "king" + squareId;
-        } else if (squareId === "1f") {
-          id = "bishop" + squareId;
-        } else if (squareId === "1g") {
-          id = "knight" + squareId;
-        } else if (squareId === "1h") {
-          id = "rook" + squareId;
-        } else if (squareId.includes("2")) {
-          id = "pawn" + squareId;
-        } else if (squareId.includes("7")) {
-          id = "pawn" + squareId;
-          isWhite = false;
-        } else if (squareId === "8a") {
-          id = "rook" + squareId;
-          isWhite = false;
-        } else if (squareId === "8b") {
-          id = "knight" + squareId;
-          isWhite = false;
-        } else if (squareId === "8c") {
-          id = "bishop" + squareId;
-          isWhite = false;
-        } else if (squareId === "8d") {
-          id = "queen" + squareId;
-          isWhite = false;
-        } else if (squareId === "8e") {
-          id = "king" + squareId;
-          isWhite = false;
-        } else if (squareId === "8f") {
-          id = "bishop" + squareId;
-          isWhite = false;
-        } else if (squareId === "8g") {
-          id = "knight" + squareId;
-          isWhite = false;
-        } else if (squareId === "8h") {
-          id = "rook" + squareId;
-          isWhite = false;
-        }
-  
-        if (id.includes("pawn")) {
-          type = pieceTypes.Pawn;
-        } else if (id.includes("rook")) {
-          type = pieceTypes.Rook;
-        } else if (id.includes("knight")) {
-          type = pieceTypes.Knight;
-        } else if (id.includes("bishop")) {
-          type = pieceTypes.Bishop;
-        } else if (id.includes("queen")) {
-          type = pieceTypes.Queen;
-        } else if (id.includes("king")) {
-          type = pieceTypes.King;
-        }
-  
-        if (!(index % 8 === 0)) {
-          boardIdLetterIndex++;
-        } else {
-          boardIdNumbersIndex++;
-          boardIdLetterIndex = 0;
-        }
-    
-        pieces.push({ id, isWhite, type, positionY, positionX }); 
-      };
-  
-      return pieces;
-    },
-    [pieces, pieceTypes]
-  );
 
   useEffect(() => {
     if (!initialized) {
-      setPieces([getPiecesDefaultsquareId()]);
+      setPieces([getStartPosition]);
       setInitialized(true);
     }
     
-  }, [initialized, getPiecesDefaultsquareId]);
+  }, [initialized]);
 
   const getSquersInPath = (pathTotalSteps, absoluteDifferenceY, absoluteDifferenceX, currentPositionY, currentPositionX) => {
     const squaresInPath = [];
---
+++ src/components/game/GameUtils.js
@@ -0,0 +1,101 @@
+export const pawnRules = (currentPosition, nextPosition, isWhite) => {
+  if (isWhite) {
+    if (currentPosition.y < nextPosition.y && currentPosition.x === nextPosition.x) {
+        if (currentPosition.y === 2 && nextPosition.y <= 4) {
+          return true;
+        } else if ((currentPosition.y + 1) === nextPosition.y) {
+          return true;
+        }
+      }
+
+      return false;
+  } else {
+    if (currentPosition.y > nextPosition.y && currentPosition.x === nextPosition.x) {
+      if (currentPosition.y === 7 && nextPosition.y >= 5) {
+        return true;
+      } else if ((currentPosition.y - 1) === nextPosition.y) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+};
+
+export const knightRules = (yDifference, xDifference) => {
+  if ((xDifference === 2 && yDifference === 1) || (yDifference === 2 && xDifference === 1)) {
+    return true;
+  }
+
+  return false;
+};
+
+export const bishopRules = (yDifference, xDifference) => {
+  const slope = Math.abs(yDifference / xDifference);
+
+  if (slope === 1) {
+    return true;
+  }
+
+  return false;
+};
+
+export const kingRules = (yDifference, xDifference) => {
+  if ((yDifference === 1 && xDifference === 0) || (xDifference === 1 && yDifference === 0) || (xDifference === 1 && yDifference === 1)) {
+    return true;
+  }
+
+  return false;
+};
+
+export const rookRules = (yDifference, xDifference) => {
+  if (yDifference === 0 || xDifference === 0) {
+    return true;
+  }
+
+  return false;
+};
+
+export const getStartPosition = [
+  {"id":"rook8a", "isWhite":false, "type":2, "positionY":8, "positionX":1},
+  {"id":"knight8b","isWhite":false,"type":3,"positionY":8,"positionX":2},
+  {"id":"bishop8c","isWhite":false,"type":4,"positionY":8,"positionX":3},
+  {"id":"queen8d","isWhite":false,"type":5,"positionY":8,"positionX":4},
+  {"id":"king8e","isWhite":false,"type":6,"positionY":8,"positionX":5},
+  {"id":"bishop8f","isWhite":false,"type":4,"positionY":8,"positionX":6},
+  {"id":"knight8g","isWhite":false,"type":3,"positionY":8,"positionX":7},
+  {"id":"rook8h","isWhite":false,"type":2,"positionY":8,"positionX":8},
+  {"id":"pawn7a","isWhite":false,"type":1,"positionY":7,"positionX":1},
+  {"id":"pawn7b","isWhite":false,"type":1,"positionY":7,"positionX":2},
+  {"id":"pawn7c","isWhite":false,"type":1,"positionY":7,"positionX":3},
+  {"id":"pawn7d","isWhite":false,"type":1,"positionY":7,"positionX":4},
+  {"id":"pawn7e","isWhite":false,"type":1,"positionY":7,"positionX":5},
+  {"id":"pawn7f","isWhite":false,"type":1,"positionY":7,"positionX":6},
+  {"id":"pawn7g","isWhite":false,"type":1,"positionY":7,"positionX":7},
+  {"id":"pawn7h","isWhite":false,"type":1,"positionY":7,"positionX":8},
+  {"id":"pawn2a","isWhite":true,"type":1,"positionY":2,"positionX":1},
+  {"id":"pawn2b","isWhite":true,"type":1,"positionY":2,"positionX":2},
+  {"id":"pawn2c","isWhite":true,"type":1,"positionY":2,"positionX":3},
+  {"id":"pawn2d","isWhite":true,"type":1,"positionY":2,"positionX":4},
+  {"id":"pawn2e","isWhite":true,"type":1,"positionY":2,"positionX":5},
+  {"id":"pawn2f","isWhite":true,"type":1,"positionY":2,"positionX":6},
+  {"id":"pawn2g","isWhite":true,"type":1,"positionY":2,"positionX":7},
+  {"id":"pawn2h","isWhite":true,"type":1,"positionY":2,"positionX":8},
+  {"id":"rook1a","isWhite":true,"type":2,"positionY":1,"positionX":1},
+  {"id":"knight1b","isWhite":true,"type":3,"positionY":1,"positionX":2},
+  {"id":"bishop1c","isWhite":true,"type":4,"positionY":1,"positionX":3},
+  {"id":"queen1d","isWhite":true,"type":5,"positionY":1,"positionX":4},
+  {"id":"king1e","isWhite":true,"type":6,"positionY":1,"positionX":5},
+  {"id":"bishop1f","isWhite":true,"type":4,"positionY":1,"positionX":6},
+  {"id":"knight1g","isWhite":true,"type":3,"positionY":1,"positionX":7},
+  {"id":"rook1h","isWhite":true,"type":2,"positionY":1,"positionX":8},
+];
+
+export const pieceTypes = {
+  Pawn: 1,
+  Rook: 2,
+  Knight: 3,
+  Bishop: 4,
+  Queen: 5,
+  King: 6
+};
