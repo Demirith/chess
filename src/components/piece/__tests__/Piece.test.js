749c4b0b65753d45cb21a399d565a767467808c8
James Demir <james.erik.demir@gmail.com>
2024-01-26 13:22:30 +0100

    Added more test cases for path validation

--- src/components/piece/__tests__/Piece.test.js
+++ src/components/piece/__tests__/Piece.test.js
@@ -13,49 +13,188 @@ describe.skip("Piece", () => {
 });
 
 describe("Path validation", () => {
-    test("Should not be a valid move if piece is in path and moving vertical", () => {
-        //Arrange
-        const currentPosition = { y: 1, x: 1 }
-        const nerxtPostition = { y: 3, x: 1 }
+    describe("Vertical", () => {
+        test("Should not be a valid move if piece is in path and moving vertical up", () => {
+            //Arrange
+            const currentPosition = { y: 1, x: 1 }
+            const nextPostition = { y: 3, x: 1 }
+    
+            const pieces = [getStartPosition]; // create test senario 
+            let validMove = true;
+    
+            //Act 
+            validMove = checkPath(currentPosition, nextPostition, pieces);
+            
+            //Assert
+            expect(validMove).toBe(false);
+        });
+    
+        test("Should be valid move if no piece is in path and moving vertical down", () => {
+            //Arrange
+            const currentPosition = { y: 3, x: 1 }
+            const nextPostition = { y: 6, x: 1 }
+    
+            const pieces = [getStartPosition]; // create test senario 
+            let validMove = true;
+    
+            //Act 
+            validMove = checkPath(currentPosition, nextPostition, pieces);
+            
+            //Assert
+            expect(validMove).toBe(true);
+        });
+    })
 
-        const pieces = [getStartPosition]; // create test senario 
-        let validMove = true;
+    describe("horizontal", () => {
+        test("Should not be a valid move if piece is in path and moving horizontal to the right", () => {
+            //Arrange
+            const currentPosition = { y: 1, x: 1 }
+            const nextPostition = { y: 1, x: 3 }
+    
+            const pieces = [getStartPosition]; // create test senario 
+            let validMove = true;
+    
+            //Act 
+            validMove = checkPath(currentPosition, nextPostition, pieces);
+            
+            //Assert
+            expect(validMove).toBe(false);
+        });
+    
+        test("Should not be a valid move if piece is in path and moving horizontal to the left", () => {
+            //Arrange
+            const currentPosition = { y: 1, x: 3 }
+            const nextPostition = { y: 1, x: 1 }
+    
+            const pieces = [getStartPosition]; // create test senario 
+            let validMove = true;
+    
+            //Act 
+            validMove = checkPath(currentPosition, nextPostition, pieces);
+            
+            //Assert
+            expect(validMove).toBe(false);
+        });
+    
+        test("Should be a valid move if no piece is in path and moving horizontal to the right", () => {
+            //Arrange
+            const currentPosition = { y: 3, x: 1 }
+            const nextPostition = { y: 3, x: 3 }
+    
+            const pieces = [getStartPosition]; // create test senario 
+            let validMove = true;
+    
+            //Act 
+            validMove = checkPath(currentPosition, nextPostition, pieces);
+            
+            //Assert
+            expect(validMove).toBe(true);
+        });
 
-        //Act 
-        validMove = checkPath(currentPosition, nerxtPostition, pieces);
-        
-        //Assert
-        expect(validMove).toBe(false);
-    });
+        test("Should be a valid move if no piece is in path and moving horizontal to the left", () => {
+            //Arrange
+            const currentPosition = { y: 3, x: 3 }
+            const nextPostition = { y: 3, x: 1 }
+    
+            const pieces = [getStartPosition]; // create test senario 
+            let validMove = true;
+    
+            //Act 
+            validMove = checkPath(currentPosition, nextPostition, pieces);
+            
+            //Assert
+            expect(validMove).toBe(true);
+        });
+    })
+    describe("diagonally", () => {
+        test("Should not be a valid move if piece is in path and moving diagonally from y1, x3 to y:3, x5 and there is a piece at y2, x4", () => {
+            //Arrange
+            const currentPosition = { y: 1, x: 3 }
+            const nextPostition = { y: 3, x: 5 }
+    
+            const pieces = [getStartPosition];  
+            let validMove = true;
+    
+            //Act 
+            validMove = checkPath(currentPosition, nextPostition, pieces);
+            
+            //Assert
+            expect(validMove).toBe(false);
+        });
 
-    test("Should not be a valid move if piece is in path and moving horizontal", () => {
-        //Arrange
-        const currentPosition = { y: 1, x: 1 }
-        const nerxtPostition = { y: 1, x: 3 }
+        test("Should not be a valid move if piece is in path and moving diagonally from y1, x3 to y:3, x1 and there is a piece at y2, x2", () => {
+            //Arrange
+            const currentPosition = { y: 1, x: 3 }
+            const nextPostition = { y: 3, x: 1 }
+    
+            const pieces = [getStartPosition];  
+            let validMove = true;
+    
+            //Act 
+            validMove = checkPath(currentPosition, nextPostition, pieces);
+            
+            //Assert
+            expect(validMove).toBe(false);
+        });
 
-        const pieces = [getStartPosition]; // create test senario 
-        let validMove = true;
+        test("Should be a valid move if no piece is in path and moving diagonally from y5, x3 to y:3, x5", () => {
+            //Arrange
+            const currentPosition = { y: 5, x: 3 }
+            const nextPostition = { y: 3, x: 5 }
+    
+            const pieces = [getStartPosition];  
+            let validMove = true;
+    
+            //Act 
+            validMove = checkPath(currentPosition, nextPostition, pieces);
+            
+            //Assert
+            expect(validMove).toBe(true);
+        });
 
-        //Act 
-        validMove = checkPath(currentPosition, nerxtPostition, pieces);
-        
-        //Assert
-        expect(validMove).toBe(false);
-    });
+        test("Should be a valid move if no piece is in path and moving diagonally from y5, x3 to y:3, x1", () => {
+            //Arrange
+            const currentPosition = { y: 5, x: 3 }
+            const nextPostition = { y: 3, x: 1 }
+    
+            const pieces = [getStartPosition];  
+            let validMove = true;
+    
+            //Act 
+            validMove = checkPath(currentPosition, nextPostition, pieces);
+            
+            //Assert
+            expect(validMove).toBe(true);
+        });
 
+        test("Should be a valid move if no piece is in path and moving diagonally from y3, x3 to y:5, x1", () => {
+            //Arrange
+            const currentPosition = { y: 3, x: 3 }
+            const nextPostition = { y: 5, x: 1 }
+    
+            const pieces = [getStartPosition];  
+            let validMove = true;
+    
+            //Act 
+            validMove = checkPath(currentPosition, nextPostition, pieces);
+            
+            //Assert
+            expect(validMove).toBe(true);
+        });
 
-    test("Should not be a valid move if piece is in path and moving diagonally from y1, x3 to y:3, x5 and there is a piece at y2, x4", () => {
-        //Arrange
-        const currentPosition = { y: 1, x: 3 }
-        const nerxtPostition = { y: 3, x: 5 }
-
-        const pieces = [getStartPosition];  
-        let validMove = true;
-
-        //Act 
-        validMove = checkPath(currentPosition, nerxtPostition, pieces);
-        
-        //Assert
-        expect(validMove).toBe(false);
-    });
+        test("Should be a valid move if no piece is in path and moving diagonally from y3, x3 to y:5, x5", () => {
+            //Arrange
+            const currentPosition = { y: 3, x: 3 }
+            const nextPostition = { y: 5, x: 5 }
+    
+            const pieces = [getStartPosition];  
+            let validMove = true;
+    
+            //Act 
+            validMove = checkPath(currentPosition, nextPostition, pieces);
+            
+            //Assert
+            expect(validMove).toBe(true);
+        });
+    })
 });
